5d4c784e5318b654e4f8141c6be9c8e00e571036
Anirban Dutta <dutta.an@northeastern.edu>
2024-05-22 01:47:54 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 import sys
 import os
+import zlib
 
+def printContentOfFile(totalContent):
+    if b'\x00' in totalContent:
+        header, contentOfFile = totalContent.split(b'\x00')
+        decodedContent = contentOfFile.decode('utf-8')
+        print(decodedContent.strip(), end='') 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -16,6 +17,15 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    if command == "cat-file":
+        if sys.argv[2] == "-p" and len(sys.argv[3]) == 40 :
+            blob_sha = sys.argv[3]
+            blob_folder = blob_sha[:2]
+            blob_hash = blob_sha[2:]
+            path = ".git/objects/" + blob_folder + "/" + blob_hash
+            with open(path, 'rb') as f:
+                totalContent = zlib.decompress(f.read())
+                printContentOfFile(totalContent)       
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
