1fa647ea2c094bd7533260bce7d91e25e99e1fd5
Anirban Dutta <dutta.an@northeastern.edu>
2024-05-23 00:55:17 -0400

    create a blob object

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 
 def printContentOfFile(totalContent):
     if b'\x00' in totalContent:
@@ -18,17 +19,30 @@ def main():
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
     if command == "cat-file":
-        if sys.argv[2] == "-p" and len(sys.argv[3]) == 40 :
+        if sys.argv[2] == "-p" and len(sys.argv[3]) == 40:
             blob_sha = sys.argv[3]
             blob_folder = blob_sha[:2]
             blob_hash = blob_sha[2:]
             path = ".git/objects/" + blob_folder + "/" + blob_hash
-            with open(path, 'rb') as f:
+            with open(path, "rb") as f:
                 totalContent = zlib.decompress(f.read())
-                printContentOfFile(totalContent)       
+                printContentOfFile(totalContent)
+    if command == "hash-object":
+        if sys.argv[2] == "-w" and len(sys.argv[3]) != 0:
+            file_name = sys.argv[3]
+            with open(file_name, 'rb') as f:
+                contentOfFile = f.read()
+                contentSHA = b"blob" + b" " + str(len(contentOfFile)).encode('utf-8') + b"\0" + contentOfFile
+                sha1_hash = hashlib.sha1()
+                sha1_hash.update(contentSHA)
+                hash_output = sha1_hash.hexdigest()
+                os.mkdir(".git/objects/" + hash_output[:2])
+                sha_file_name = os.path.join(".git/objects/", hash_output[:2], hash_output[2:])
+                with open(sha_file_name, 'wb')  as f:
+                    f.write(zlib.compress(contentSHA))
+                    print(hash_output, end='')
     else:
         raise RuntimeError(f"Unknown command #{command}")
-
-
+        
 if __name__ == "__main__":
     main()
